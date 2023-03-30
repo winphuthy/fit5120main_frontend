5c377a56fed064cecdd1b8bd4bcfb1c9220bc540
Lester <245252159@qq.com>
2023-03-30 11:18:44 +1100

    init project runable version

---
+++ src/page/HomePage.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import AppAppBar from "../templates/modules/views/AppAppBar";
+import ProductHero from "../templates/modules/views/ProductHero";
+import ProductValues from "../templates/modules/views/ProductValues";
+import ProductCategories from "../templates/modules/views/ProductCategories";
+import ProductHowItWorks from "../templates/modules/views/ProductHowItWorks";
+import ProductCTA from "../templates/modules/views/ProductCTA";
+import ProductSmokingHero from "../templates/modules/views/ProductSmokingHero";
+import AppFooter from "../templates/modules/views/AppFooter";
+
+export default function HomePage() {
+    return (
+        // <AppTheme title="Onepirate theme - MUI" description="A onepirate theme">
+        <div>
+            <React.Fragment>
+                <AppAppBar />
+                <ProductHero />
+                <ProductValues />
+                <ProductCategories />
+                <ProductHowItWorks />
+                <ProductCTA />
+                <ProductSmokingHero />
+                <AppFooter />
+            </React.Fragment>
+        </div>
+        // </AppTheme>
+    );
+}
