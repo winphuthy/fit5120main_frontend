726458a79a036a92de1bae4f342dd1170b624e2c
Lester <245252159@qq.com>
2023-04-03 10:43:14 +1000

    demo week1 ver. 2.0 basic done

---
+++ src/components/sidebar/DigitalCommunicationSidebar.jsx
@@ -0,0 +1,18 @@
+import './sidebar.css'
+
+export const DigitalCommunicationSidebar = () => {
+    return (
+        <div>
+            <div className="sidebar">
+                <ul>
+                    <hr/>
+                    <li>Installing Whatsapp</li>
+                    <li>Setting up Skype call</li>
+                    <li>How to use FaceTime</li>
+                    <hr/>
+                </ul>
+            </div>
+        </div>
+
+    )
+};
---
+++ src/components/sidebar/GetInvolvedSidebar.jsx
@@ -0,0 +1,19 @@
+import './sidebar.css'
+
+export const GetInvolvedSidebar = () => {
+    return (
+        <div>
+            <div className="sidebar">
+                <ul>
+                    <h3>Donations</h3>
+                    <hr/>
+                    <li>Donating a laptop</li>
+                    <li>Donating a phone</li>
+                    <hr/>
+                    <li>Other E-waste</li>
+                </ul>
+            </div>
+        </div>
+
+    )
+};
