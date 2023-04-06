cb0eb54314dab041226db2d499a4a6bb969bdbcd
XiongjieHuang <xiongjiemel19@gmail.com>
2023-04-06 19:48:01 +0800

    fix communication

--- src/page/Communication.jsx
+++ src/page/Communication.jsx
@@ -77,6 +77,8 @@ export const Commnication = () => {
                     <div>
                         <h2>Info on setting up Zoom:</h2>
                         <p>Zoom is a video conferencing platform that enables users to hold group calling sessions. It can be used for video and audio meetings, screen sharing, recording, and just simply chatting. Zoom is most commonly used for conference calls, but is also oftentimes used for large family calls, especially for including overseas family members.</p>
+                        <p>Step 1: On your Web browser, navigate to the top of it, and click the “+”icon to open a new tab </p>
+                        <p>Step 2: Within the empty address bar at the top of the tab, enter the following link: “https://www.zoom.us/DOWNLOAD ”.  </p>
                     </div>
                     <div style={{margin: "30px 0px"}}>
                         <StepContent content={settingZoom} />
