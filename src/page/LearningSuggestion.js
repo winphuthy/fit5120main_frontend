a1154651c4bd6f2cd696351f281368bb8b556354
XiongjieHuang <xiongjiemel19@gmail.com>
2023-04-22 11:07:45 +0800

    fix some ui

--- src/page/LearningSuggestion.js
+++ src/page/LearningSuggestion.js
@@ -1,11 +1,19 @@
 import './About.css';
 import KeywordImage from '../images/keywordPage.jpeg';
 import SuggestionImage from '../images/learningsuggestions.jpg'
-import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 
 export function LearningSuggestion() {
-  const [inputValue, setInputValue] = useState('');
+  const councilList = [
+    { label: 'Word 1', weight: 0, category: 'landfill' },
+    { label: 'Word 2', weight: 0, category:  'recyclable'  },
+    { label: 'Word 3', weight: 0, category: 'landfill'  },
+    { label: 'Word 4', weight: 0, category: 'organic' },
+   
+];
+
     return (
         <div style={{backgroundImage:`url(${SuggestionImage})`,backgroundRepeat:"no-repeat",height: "auto", paddingTop: '50px', paddingBottom: '50px',backgroundSize: 'cover'}}>
         <div style={{width: '70%', margin: "auto", color: "black",display: 'flex',justifyContent: 'center',alignItems: 'center'}}>
@@ -22,11 +30,13 @@ export function LearningSuggestion() {
       </div>
       <h1></h1>
       <div style={{position: 'relative', width: '25%',margin: 'auto'}}>
-  <select value={inputValue} onChange={(e) => setInputValue(e.target.value)} style={{ height:50, width:'100%',backgroundColor: 'rgba(220, 220, 220, 0.2)', color: 'black',fontWeight: 'bolder',fontSize: '1.3rem',margin:'auto'}}>
-    <option value="word 1">word 1</option>
-    <option value="word 2">word 2</option>
-    <option value="word 3">word 3</option>
-  </select>
+          <Autocomplete
+            disablePortal
+            id="word-list"
+            options={councilList}
+            sx={{ width: 400,marginLeft:'3%',backgroundColor:"darkgrey"}}
+            renderInput={(params) => <TextField {...params} label="List of word" />}
+        />
 </div>
       <h1></h1>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
