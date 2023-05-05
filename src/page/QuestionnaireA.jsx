4aca8f7dd5dd6d41aab7256b9025c527fcccccda
XiongjieHuang <xiongjiemel19@gmail.com>
2023-05-05 13:27:34 +0800

    change ui

--- src/page/QuestionnaireA.jsx
+++ src/page/QuestionnaireA.jsx
@@ -1,12 +1,40 @@
 import MainImage from '../images/mainpage.jpg'
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 
 export const QuestionnaireA = () => {
-    
 
+    const [selectedOption, setSelectedOption] = React.useState("");
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    const handleButtonClick = () => {
+        switch(selectedOption) {
+            case "option1":
+                window.location.href = 'email';
+                break;
+            case "option2":
+                window.location.href = 'mygov';
+                break;
+            case "option3":
+                window.location.href = 'mygov';
+                break;
+            case "option4":
+                window.location.href = 'medicare';
+                break;
+            case "option5":
+                window.location.href = 'learningsuggestions';
+                break;
+            default:
+                break;
+        }
+    };
+    
 
     return (
         <div style={{
@@ -29,15 +57,28 @@ export const QuestionnaireA = () => {
                     <h1>Digital Service</h1>
                 </div>
                 <div style={{width: '50vw', margin: 'auto'}}>
-                    <p style={{textAlign: 'justify', fontSize: '1.2rem'}}>Questionnaire Survey</p>
+                    
+                    <p style={{textAlign: 'justify', fontSize: '1.2rem'}}>Questionnaire A</p>
                     <hr style={{width: '50vw', marginTop: '50px', marginBottom: "50px"}}/>
-
-
-  
+                    <p style={{textAlign: 'justify', fontSize: '1.2rem'}}>Text</p>
+                    <hr style={{width: '50vw', marginTop: '50px', marginBottom: "50px"}}/>
+                    <div style={{backgroundColor: 'cornsilk', padding: '20px'}}>
+                        <div style={{backgroundColor: 'grey', padding: '20px', marginBottom: '20px'}}>
+                            <p>Question: Which of the following best describes your need?</p>
+                            <FormControlLabel control={<Checkbox value="option1" onChange={handleOptionChange} />} label="A. I want to receive and send email." />
+                            <br />
+                            <FormControlLabel control={<Checkbox value="option2" onChange={handleOptionChange} />} label="B. I want to check out Super summary for this financial year." />
+                            <br />
+                            <FormControlLabel control={<Checkbox value="option3" onChange={handleOptionChange} />} label="C. I want to access government services online." />
+                            <br />
+                            <FormControlLabel control={<Checkbox value="option4" onChange={handleOptionChange} />} label="D. I want to check my Medicare claims and statements." />
+                            <br />
+                            <FormControlLabel control={<Checkbox value="option5" onChange={handleOptionChange} />} label="E. Not of above." />
+                            <br />
+                            <Button variant="contained" sx = {{width:'150px',height:'50px',backgroundColor: 'aliceblue',fontsize:'1.5rem',fontWeight: 'bolder',display: 'block',margin:'auto'}} onClick={handleButtonClick}>submit</Button>                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        
-    )
-
+    );
 }
